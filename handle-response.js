e1722fef70ae3e9c468274c7ced54f6dc7f3d2a6
Buwaneka Sumanasekara <buwanekasumanasekara@gmail.com>
2023-04-04 15:26:44 +0530

    initial commit

---
+++ handle-response.js
@@ -0,0 +1,171 @@
+const axios = require("axios").default;
+
+const MESSAGE_TYPE = {
+  TEXT: "text",
+  INTERACTIVE: "interactive",
+};
+const KEYWORD = "HELLO";
+
+const phoneNumber = "119940174385529";
+const URL = `https://graph.facebook.com/v16.0/119940174385529/messages`;
+const token =
+  "EAAKWugvGI7UBAJrMoM2ndfyBZAuNd44exC2COZBr82MKNM8Czx7rgpuURmZBT4oZCuv25urrs6bdvQrhM943CgQPukbr0ltH5t5TJK7gCaxc9i194CO4ZAWMm9U79fnxnE44yqS5CdZAdA5oZBZB5UI30ifh5vXP69DDWuzZBZCzq8ZAjvA9IlKScBnj44IZCMvGe0olWFY3KP9KWrhIp2FmZAZA2i";
+
+const office = "BANGLADESH  DHAKA";
+const questions = [
+  {
+    question: "Please advice on stability on Air Freight Rate Changes",
+    answers: [
+      { id: 1, title: "No Change" },
+      { id: 2, title: "Rates Increased" },
+      { id: 3, title: "Rates Decreased" },
+    ],
+  },
+];
+
+
+const messageType="button";
+
+
+function processResponseMessage(value) {
+  const contacts = value.contacts;
+  const messageCount = value.messages.length || 0;
+  
+ 
+
+  if (messageCount > 0) {
+    const lastMessage = value.messages[messageCount - 1];
+    const contact = contacts.find((v) => v.wa_id === lastMessage.from);
+    if (lastMessage.type === MESSAGE_TYPE.TEXT) {
+      const txt = lastMessage.text.body || "";
+      if (KEYWORD === txt.toUpperCase()) {
+        handleGreetingMessage(contact);
+        
+        setTimeout(()=>sendQuestion(contact, 0),200)
+        
+      } else {
+        console.error("unsupported keyword:", txt);
+      }
+    } else if (lastMessage.type === MESSAGE_TYPE.INTERACTIVE) {
+      handleInteractiveMessageResp(contact, lastMessage);
+    } else {
+      throw Error("Un supported message type");
+    }
+  } else {
+    throw Error("No messages found");
+  }
+}
+
+const reqHeader = {
+  headers: {
+    Authorization: `Bearer ${token}`,
+    "Content-Type": "application/json",
+  },
+};
+
+function sendQuestion(contact, questionIndex = 0) {
+  console.log("send initial question to:", contact.wa_id);
+
+  const userName = contact.profile.name;
+  const questionObj = questions[questionIndex];
+
+  const buttons = questionObj.answers.map((a) => ({
+    type: "reply",
+    reply: a,
+  }));
+
+  
+
+  const reqBody = {
+    messaging_product: "whatsapp",
+    recipient_type: "individual",
+    to: contact.wa_id,
+    type: MESSAGE_TYPE.INTERACTIVE,
+    interactive: {
+      header: {
+        type: "text",
+        text: office,
+      },
+      type: messageType,
+      body: {
+        text: questionObj.question,
+      },
+      action: {
+        buttons: buttons,
+      },
+    },
+  };
+
+
+  axios
+    .post(URL, reqBody, reqHeader)
+    .then((response) => {
+      const res=response.data
+      
+      const messagesCount=res.messages.length || 0
+      
+      if(messagesCount>0){
+        console.log("send message Id:",res.messages[messagesCount-1].id);
+      }
+      
+    })
+    .catch((error) => {
+      console.log("error---", error.response.data);
+    });
+}
+
+function handleGreetingMessage(contact) {
+  const userName = contact.profile.name;
+  const reqBody = {
+    messaging_product: "whatsapp",
+    recipient_type: "individual",
+    to: contact.wa_id,
+    type: "text",
+    text: {
+      preview_url: false,
+      body: `Hello ${userName},`,
+    },
+  };
+
+  axios
+    .post(URL, reqBody, reqHeader)
+    .then((response) => {
+      //console.log(response.data);
+    })
+    .catch((error) => {
+      console.log("error---", error.response.data);
+    });
+}
+
+function handleInteractiveMessageResp(contact, message) {
+  
+  const messageId=message.context.id
+  
+
+  
+  const response = message.interactive.button_reply;
+  
+    console.log("response received for messageId :",messageId, "   resp:",JSON.stringify(response))
+
+  const reqBody = {
+    messaging_product: "whatsapp",
+    recipient_type: "individual",
+    to: contact.wa_id,
+    type: "text",
+    text: {
+      preview_url: false,
+      body: `Thank you for your response. We will record the response as : ${response.title}`,
+    },
+  };
+
+  axios
+    .post(URL, reqBody, reqHeader)
+    .then((response) => {
+      //console.log(response.data);
+    })
+    .catch((error) => {
+      console.log("error---", error.response.data);
+    });
+}
+
+module.exports = { processResponseMessage };
